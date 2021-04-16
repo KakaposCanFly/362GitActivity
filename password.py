943d3049a08b7fc0f090ae5c32578f4374b0b25c
Cole Theodore <theodoco@flip1.engr.oregonstate.edu>
2021-04-16 14:47:31 -0700

    finished for loop and password generation

--- password.py
+++ password.py
@@ -3,4 +3,5 @@ import random
 def generatePassword(length):
     password = ['a'] * length
     for i in range(0, length):
-        pass
+        password[i] = chr(random.randint(32, 126))
+        print(password[i], end = '')
