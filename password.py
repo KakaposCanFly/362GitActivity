97a278a06bea8468bad76810a1721f202da215a3
Cole Theodore <theodoco@flip1.engr.oregonstate.edu>
2021-04-16 14:49:37 -0700

    added user input and error handling

--- password.py
+++ password.py
@@ -5,3 +5,11 @@ def generatePassword(length):
     for i in range(0, length):
         password[i] = chr(random.randint(32, 126))
         print(password[i], end = '')
+
+num = 0
+while(1):
+    try:
+        num = int(input("Enter the password length: "))
+        break
+    except:
+        print("Invalid input")
